c1c52e53c097ac633dd77bedb3456ff0f1600c26
parag jagdale <55179496+imparag28@users.noreply.github.com>
2021-09-12 13:09:37 +0000

    day11

--- Sep11_day12/p2.js
+++ Sep11_day12/p2.js
@@ -15,11 +15,11 @@ let string = ""
 for(i=1;i<=5;i++){
   for(j=1;j<=5;j++){
    
-    if( i== 1 || i == 5 ||){
+     if(i==1 || i==5 || j==5 || j==1){
        string +="*"
+    }else{
+      string +=" "
     }
-    
-  
   }
    string += "\n";
 }
