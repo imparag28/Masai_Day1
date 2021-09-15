956fbfca7a64dd5d3a183c30e2e52e2c638eea9e
parag jagdale <55179496+imparag28@users.noreply.github.com>
2021-09-15 18:16:03 +0000

    new changes

---
+++ s15_day17/problem.js
@@ -0,0 +1,6 @@
+
+    // Given a string count the number of words in that string
+    // Given a string in lower case convert it to upper case
+    // Given an array of string count the overall total number of characters
+
+
